d6b84b24efb888e3f7014757c38a51da72f4c03e
Stoppedwumm <129097720+Stoppedwumm@users.noreply.github.com>
2024-02-08 15:17:20 +0000

    More secret recipes

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express")
-require('dotenv').config()
+console.log(process.env["PASSWORDID"])
+if (process.env["PASSWORDID"] == undefined) {
+    require('dotenv').config()
+} 
 const app = express()
 const password = process.env["PASSWORDID"]
+var cookies = require("cookie-parser")
+
+app.use(cookies())
 
 console.log(password)
 
@@ -18,7 +24,17 @@ app.get("/early-access-login", (req, res) => {
 app.get("/api/loginapi", (req,res) => {
     const pass = req.query.loginnumber
     if (pass == password) {
+        res.cookie("LoggedIn", password)
         res.sendFile(__dirname + "/afterlogin.html")
+    }
+})
+
+app.get("/therealantidodo", (req, res) => {
+    const cookie = req.cookies["LoggedIn"]
+    if (cookie == password) {
+        res.sendFile(__dirname + "/dashboard.html")
+    } else {
+        res.status(401).send("<error><h1>401: Unauthorized</h1></error>")
     }
 })
 
